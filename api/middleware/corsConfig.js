89a6574df8d950267d8bafa8322e50764a884621
vuthanhdat041 <dathanh04@gmail.com>
2025-06-16 16:28:51 +0700

    sai url trên cors

--- api/middleware/corsConfig.js
+++ api/middleware/corsConfig.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-    origin: 'ban-hang-2-0-k3ld85ze8-vuthanhdat041s-projects.vercel.app', // Cập nhật domain của React app
+    origin: 'https://ban-hang-2-0.vercel.app', // Cập nhật domain của React app
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Các phương thức được phép
     allowedHeaders: ['Content-Type', 'Authorization'], // Header được phép
     credentials: true // Cho phép gửi cookie qua CORS
